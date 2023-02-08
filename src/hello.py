03987188a4ae04b6bd19055406c19facab6bf5c9
AhmedAlFeQy <95620707+AhmedAlFeQy@users.noreply.github.com>
2023-02-08 20:46:08 +0200

    Update hello.py

--- src/hello.py
+++ src/hello.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, Botit! Hi Hello feki '
+    return 'Hello, Botit! Hi hi Hello feki '
 
 @app.route('/<page_name>')
 def other_page(page_name):
